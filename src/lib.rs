a49c0649a91c6fb31bd9aa81322287a95fb9fa15
James Parker <dev@jamesparker.me>
2017-11-15 18:31:05 -0500

    init

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+
+#[no_mangle]
+pub extern fn hello_rust() -> *const u8 {
+    "Hello, world!\0".as_ptr()
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
